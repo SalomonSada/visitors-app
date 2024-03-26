337d6a537fc8ca97eb664436052c446e89afb7b5
Salomon Sada <sajosaca@gmail.com>
2024-03-25 23:24:17 -0300

    ajuste para deploy en render

--- server.js
+++ server.js
@@ -28,3 +28,5 @@ if (process.env.NODE_ENV === 'production') {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
+
+//"heroku-postbuild": "NPM_CONFIG_PRODUCTION=false npm install --prefix client && npm run build --prefix client"
